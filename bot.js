61a41ac6935854acfef84e7d0c15e428ec2c6616
Steffen Triekels <steffentriekels@gmail.com>
2017-08-22 01:06:27 +0200

    Rollback

--- bot.js
+++ bot.js
@@ -12,17 +12,11 @@ else {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+bot.onText(/^/, function (msg) {
+  var name = msg.from.first_name;
+  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(function () {
+    // reply sent!
+  });
 });
 
 module.exports = bot;
