46edfa2acb94dced3e8fd57c229e1ed0e995384d
Steffen Triekels <steffentriekels@gmail.com>
2017-08-22 01:36:38 +0200

    test

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 bot.onText(/^/, function (msg) {
   var name = msg.from.first_name;
   if (msg.text = 'k'){
-    bot.sendMessage(msg.chat.id, 'GOH ' + name + ', WAT EEN MOOIE LETTER!').then(function () {});
+    bot.sendMessage(msg.chat.id, 'GOH ' + name + ', WAT EEN MOOIE LETTER!' + msg.text).then(function () {});
   }
   });
 
