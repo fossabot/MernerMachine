c6bed18b77a05322a7be5022648518d7d955df1a
Steffen Triekels <steffentriekels@gmail.com>
2017-08-22 01:15:24 +0200

    Test

--- bot.js
+++ bot.js
@@ -12,12 +12,16 @@ else {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-bot.onText(/^/, function (msg) {
-  var name = msg.from.first_name;
-  if (msg="k") {
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+  
+ if (msg="k") {
     bot.sendMessage(msg.chat.id, 'GOH ' + name + ', WAT EEN MOOIE LETTER!').then(function () {
     });
   }
-  });
+});
 
 module.exports = bot;
