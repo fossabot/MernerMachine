8f9001db083614cf98a03fe85f10e28a78af1dca
Steffen Triekels <steffentriekels@gmail.com>
2017-08-22 01:04:33 +0200

    Testing replies

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 var token = process.env.TOKEN;
-
 var Bot = require('node-telegram-bot-api');
 var bot;
 
@@ -13,11 +12,17 @@ else {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-bot.onText(/^/, function (msg) {
-  var name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(function () {
-    // reply sent!
-  });
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
 
 module.exports = bot;
