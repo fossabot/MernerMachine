6dbcc51ab172809669550562b91154f70c201bf0
Steffen Triekels <steffentriekels@gmail.com>
2017-08-22 01:13:02 +0200

    Test "k" reply

--- bot.js
+++ bot.js
@@ -14,9 +14,10 @@ console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.onText(/^/, function (msg) {
   var name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(function () {
-    // reply sent!
+  if (msg="k") {
+    bot.sendMessage(msg.chat.id, 'GOH ' + name + ', WAT EEN MOOIE LETTER!').then(function () {
+    });
+  }
   });
-});
 
 module.exports = bot;
