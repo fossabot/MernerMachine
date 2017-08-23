e6f75f9ba95f775d7a16a4be89c862288823d02c
Steffen Triekels <steffentriekels@gmail.com>
2017-08-23 23:18:44 +0200

    var for msg.text

--- bot.js
+++ bot.js
@@ -14,16 +14,17 @@ else {
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 bot.onText(/^/, function (msg) {
   var name = msg.from.first_name;
-  if (msg.text.toLowerCase() =='Hello' || msg.text.toLowerCase() =='Hallo' || msg.text.toLowerCase() =='Hi' || msg.text.toLowerCase() =='Hey'){
+  var msgText = msg.text;
+  if (msgText.toLowerCase() =='Hello' || msgText.toLowerCase() =='Hallo' || msgText.toLowerCase() =='Hi' || msgText.toLowerCase() =='Hey'){
     bot.sendMessage(msg.chat.id, 'Hallo ' + name + '!');
   }
-  if (msg.text.toLowerCase() =='k'){
+  if (msgText.toLowerCase() =='k'){
     bot.sendMessage(msg.chat.id, 'EEN PRACHTIGE LETTER ' + name.toUpperCase() + '!');
   }
-  if (msg.text.toLowerCase() =='lol' && !(msg.text == "LOL")){
+  if (msgText.toLowerCase() =='lol' && !(msgText == "LOL")){
     bot.sendVideo(msg.chat.id, "https://media1.giphy.com/media/fGuqeA6PiXINa/giphy.gif");
   }
-  if (msg.text.toLowerCase().includes('goesting') || msg.text.toLowerCase().includes('zin in')){
+  if (msgText.toLowerCase().includes('goesting') || msgText.toLowerCase().includes('zin in')){
       bot.sendMessage(msg.chat.id, '( ͡° ͜ʖ ͡°)');
   }
 });
