fd4baed6ddf80bd543b49afe7d99b14990afd019
Steffen Triekels <steffentriekels@gmail.com>
2017-08-23 23:25:41 +0200

    Optimizations

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
+//Get API and token
 var token = process.env.TOKEN;
 var Bot = require('node-telegram-bot-api');
 var bot;
 
+//Set webhook
 if(process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
   bot.setWebHook(process.env.HEROKU_URL + bot.token);
@@ -10,8 +12,8 @@ else {
   bot = new Bot(token, { polling: true });
 }
 
+//Log on console
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
-
 
 bot.onText(/^/, function (msg) {
 //Create vars 
