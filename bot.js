671aa049b961baf99bc01ebd0be686da6208749d
Steffen Triekels <steffentriekels@gmail.com>
2017-08-24 21:00:10 +0200

    Extra string var

--- bot.js
+++ bot.js
@@ -15,21 +15,24 @@ else {
 //Log on console
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-bot.onText(/^/, function (msg) {
+bot.onText(/^/, function (msg){
 //Create vars 
   var name = msg.from.first_name;
   var msgText = string(msg.text);
   var chatid = string(msg.chat.id);
+  var sendString;
 
 //Responses
 if (msgText.toLowerCase()=='k'){
-  bot.sendMessage(chatid, 'EEN PRACHTIGE LETTER ' + name.toUpperCase() + '!');
+  sendString= 'EEN PRACHTIGE LETTER ' + name.toUpperCase() + '!'
+  bot.sendMessage(chatid, sendString);
 
 } else if (msgText.toLowerCase() == 'lol' && !(msgText == "LOL")){
   bot.sendVideo(chatid, "https://media1.giphy.com/media/fGuqeA6PiXINa/giphy.gif");
 
 } else if (msgText.toLowerCase().includes('goesting') || msgText.toLowerCase().includes('zin in')){
-  bot.sendMessage(chatid, '( ͡° ͜ʖ ͡°)');
+  sendString = '( ͡° ͜ʖ ͡°)';
+  bot.sendMessage(chatid, sendString);
 }
 });
 
